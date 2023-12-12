665cc4dfb7221ee26a4b0d909e0d57dc8def659e
KhizarAziz <khizar@scanbot.io>
2023-12-12 01:45:07 +0100

    added sentiment test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -41,3 +41,33 @@ def test_fibonacci_sequence(test_client):
         
         # Check if the JSON response is the expected Fibonacci number
         assert response.json() == expected_result, f"Failed for input: {input_value}"
+
+
+
+def test_text_sentiment(test_client):
+    """
+    Test the sentiment for known inputs and outputs.
+    
+    This test checks if the API returns the correct Fibonacci sequence 
+    for the given input numbers. It uses a parameterized test approach
+    to avoid redundancy and makes the test easily extendable.
+    
+    Args:
+        test_client (TestClient): Fixture for creating a test client for the API.
+    """
+
+    # Dictionary mapping input values to expected Fibonacci results
+    test_cases = {
+        "I ammm bad bad bad": 0,
+    }
+
+    # Loop through the test cases to ensure code reusability
+    for input_value, expected_result in test_cases.items():
+        # Make the GET request to the FastAPI endpoint
+        response = test_client.get(f"/{input_value}")
+        
+        # Check if the status code is as expected (200 OK)
+        assert response.status_code == 200, f"text Failed for input: {input_value}"
+        
+        # Check if the JSON response is the expected sentiment value for the input
+        assert response.json() == expected_result, f"text Failed for input: {input_value}, response was {response.json()}"
