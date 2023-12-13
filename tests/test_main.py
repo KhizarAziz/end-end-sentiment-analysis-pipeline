eb87359290bfaddcd7b052afad27a466d3f53e77
KhizarAziz <khizar@scanbot.io>
2023-12-13 02:49:10 +0100

    added test cases for sentiment analysis

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+from models.text_model import TextModel
 
 # Utilize a fixture for the TestClient
 @pytest.fixture(scope="module")
@@ -48,7 +49,7 @@ def test_text_sentiment(test_client):
     """
     Test the sentiment for known inputs and outputs.
     
-    This test checks if the API returns the correct Fibonacci sequence 
+    This test checks if the API returns the correct sentiment  
     for the given input numbers. It uses a parameterized test approach
     to avoid redundancy and makes the test easily extendable.
     
@@ -56,15 +57,21 @@ def test_text_sentiment(test_client):
         test_client (TestClient): Fixture for creating a test client for the API.
     """
 
-    # Dictionary mapping input values to expected Fibonacci results
+    # Dictionary mapping input values to expected sentiment results
     test_cases = {
-        "I ammm bad bad bad": 0,
+        "very good day": "positive",
+        "food was ok but the place is too dirty": "negative",
+        "Loved the food, but the money isnt worth it! Anyways, i am pretty happy :)": "positive",
+        "Having a family is kool, but having friends is kooler!": "positive",
+        "the day is saved by the PPG" : "positive"
+
     }
 
     # Loop through the test cases to ensure code reusability
     for input_value, expected_result in test_cases.items():
         # Make the GET request to the FastAPI endpoint
-        response = test_client.get(f"/{input_value}")
+        data = {"text": input_value}
+        response = test_client.post("/predict/",json=data)
         
         # Check if the status code is as expected (200 OK)
         assert response.status_code == 200, f"text Failed for input: {input_value}"
